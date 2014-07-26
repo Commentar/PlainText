433789489574708b523ffdda0daa3f43fb9adc77
PeeHaa <info@pieterhordijk.com>
2014-07-26 20:58:05 +0200

    Update version and year

--- src/Commentar/Post/PlainText.php
+++ src/Commentar/Post/PlainText.php
@@ -7,9 +7,9 @@
  * @category   Commentar
  * @package    Post
  * @author     Pieter Hordijk <info@pieterhordijk.com>
- * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @copyright  Copyright (c) 2014 Pieter Hordijk
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version    1.0.0
+ * @version    0.0.2
  */
 namespace Commentar\Post;
 
